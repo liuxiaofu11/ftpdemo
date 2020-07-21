01dba72a62fce9159c7dc4928eb0a4dec91e092a
11111 <1248827677@qq.com>
2020-07-21 15:31:08 +0800

    添加测试的类11111111111

---
+++ gongzuokongjian/ftpdemo/src/main/java/com/example/ftpdemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.ftpdemo.controller;
+
+/**
+ * @Author teswell
+ * @Date 2020/7/21 15:23
+ * @function
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
